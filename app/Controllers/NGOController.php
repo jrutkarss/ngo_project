2b9b4bf12e674abbcc1897b9e413a8c01f8fa3ae
UTKARSS <100163406+jrutkarss@users.noreply.github.com>
2026-04-02 12:29:01 +0530

    Add NGOController

---
+++ app/Controllers/NGOController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\NGOModel;
+
+class NGOController extends BaseController {
+    public function index() {
+        // Code to list NGOs
+    }
+
+    public function create() {
+        // Code to create an NGO
+    }
+}
